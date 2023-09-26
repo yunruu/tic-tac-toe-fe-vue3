da73637464edb19b07afe413cd3a28eef3f44820
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-26 23:13:11 +0800

    feat: add elementplus into app

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import router from "./router";
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-const app = createApp(App);
+const app = createApp(App)
 
-app.use(router);
+app.use(ElementPlus)
+app.use(router)
 
-app.mount("#app");
+app.mount('#app')
