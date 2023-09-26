58181c12bddd76dcd06c8366b9149176183fc1ea
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-26 22:49:10 +0800

    feat: add app header and styles

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
-import { RouterView } from "vue-router";
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <header class="app-header">Tic Tac Toe - Version Accessible</header>
   <RouterView />
 </template>
 
-<style scoped></style>
+<style scoped>
+.app-header {
+  background-color: #020422;
+  font-size: 16px;
+  font-weight: 600;
+  color: white;
+  padding: 15px;
+  text-align: left;
+}
+</style>
