bae118f0722fd6582395f1327cfae898b72be06a
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-27 14:28:38 +0800

    feat: add make move api call

--- src/api/game.js
+++ src/api/game.js
@@ -1,3 +1,6 @@
 import { http } from '../utils/request'
 
 export const joinGame = async (pid) => http('post', `/game`, { pid })
+
+export const makeMove = async (id, pid, boardIdx, moveValue) =>
+  http('patch', `/game/board/${id}/${pid}`, { move: { idx: boardIdx, value: moveValue } })
