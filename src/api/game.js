0a5dc8c585f92c4b6f8125783bf1f76f3a9dbc2a
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-27 18:16:41 +0800

    feat: add game info api

--- src/api/game.js
+++ src/api/game.js
@@ -2,6 +2,8 @@ import { http } from '../utils/request'
 
 export const joinGame = async (pid) => http('post', `/game`, { pid })
 
+export const boardInfo = async (id) => http('get', `/game/${id}`)
+
 export const makeMove = async (id, pid, boardIdx, moveValue) =>
   http('patch', `/game/board/${id}/${pid}`, { move: { idx: boardIdx, value: moveValue } })
 
