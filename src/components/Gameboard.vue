889fd640754b0ccd3e315e218617ac7c17b0ecc6
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-27 12:10:45 +0800

    feat: use player position to draw 'O' on its moves

--- src/components/Gameboard.vue
+++ src/components/Gameboard.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="game-board" class="mt-3">
     <div id="row-one" class="cell-row">
-      <Cell v-for="grid in rowOne" :symbol="board[grid]" :id="grid" :handleClick="handleClick" />
+      <Cell v-for="grid in rowOne" :symbol="symbol(board[grid])" :id="grid" :handleClick="handleClick" />
     </div>
     <div id="row-two" class="cell-row">
-      <Cell v-for="grid in rowTwo" :symbol="board[grid]" :id="grid" :handleClick="handleClick" />
+      <Cell v-for="grid in rowTwo" :symbol="symbol(board[grid])" :id="grid" :handleClick="handleClick" />
     </div>
     <div id="row-three" class="cell-row">
-      <Cell v-for="grid in rowThree" :symbol="board[grid]" :id="grid" :handleClick="handleClick" />
+      <Cell v-for="grid in rowThree" :symbol="symbol(board[grid])" :id="grid" :handleClick="handleClick" />
     </div>
   </div>
 </template>
@@ -25,11 +25,11 @@ const props = defineProps({
     type: Object,
     required: true,
   },
+  playerPosition: {
+    type: Number,
+    required: true,
+  },
 })
-
-const handleClick = () => {
-  console.log('clicked')
-}
 
 const rowOne = computed(() => {
   return props.board.slice(0, 3)
@@ -42,6 +42,14 @@ const rowTwo = computed(() => {
 const rowThree = computed(() => {
   return props.board.slice(6, 9)
 })
+
+const handleClick = () => {
+  console.log('clicked')
+}
+
+const symbol = (value) => {
+  return value === 0 ? '' : playerPosition === value ? 'O' : 'X'
+}
 </script>
 
 <style scoped>
