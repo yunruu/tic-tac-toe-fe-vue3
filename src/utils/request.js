8b0afcea286e38c2784dd8c42a938c8f790d346a
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-27 14:28:30 +0800

    feat: add res interceptors to redirect errors to modal

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,34 @@
 import axios from 'axios'
+import { openDialog } from './ui'
 
 let service = axios.create({
   baseURL: import.meta.env.VITE_BASE_API, // url = base url + request url
 })
+
+init()
+function init() {
+  // Response interceptors
+  service.interceptors.response.use(
+    async (response) => {
+      const res = response
+      if (res.status !== 200 && res.status !== 201) {
+        await catchError(response)
+        return Promise.reject(response)
+      } else {
+        return res
+      }
+    },
+    async (error) => {
+      await catchError(error)
+      return Promise.reject(error)
+    },
+  )
+}
+
+async function catchError(error) {
+  let message = error.response?.data?.message || error.response?.statusText || error.message
+  openDialog(message, 'Warning', 'warning', false)
+}
 
 export default service
 
