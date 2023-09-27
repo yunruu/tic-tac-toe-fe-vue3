d0940565769cc9c27771d51ba2b250e987ea1672
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-27 11:28:36 +0800

    feat: add game page base

---
+++ src/views/GamePage.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { computed, ref, onMounted, onBeforeMount } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { openDialog } from '../utils/ui'
+
+const route = useRoute()
+const router = useRouter()
+
+const gameSession = ref({
+  id: '',
+  players: [null, null],
+  turn: 0,
+  board: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  winner: null,
+})
+
+const playerInfo = ref({
+  id: '',
+  username: '',
+})
+
+onMounted(async () => {
+  try {
+    if (!route.query.player) {
+      router.push({ name: 'home' })
+    }
+    playerInfo.value = JSON.parse(decodeURIComponent(route.query.player))
+    if (!playerInfo) {
+      throw new Error()
+    }
+  } catch {
+    await openDialog('Player information not found, please try to join a game again', 'Error', 'error')
+    router.push({ name: 'home' })
+  }
+})
+
+const playerNames = computed(() => {
+  const [playerOne, playerTwo] = gameSession.value.players
+  return {
+    playerOne: playerOne?.username || '-',
+    playerTwo: playerTwo?.username || '-',
+  }
+})
+</script>
+
+<template>
+  <header>
+    <h1 class="text-7xl font-bold mt-10 mb-8 text-white text-center">Tic-Tac-Toe</h1>
+  </header>
+  <main>
+    <section class="text-center text-xl" aria-label="Game session information">
+      <p class="text-gray-400 font-semibold mb-3">Session ID: {{ gameSession.id || '-' }}</p>
+      <span class="text-white mr-4">Player 1: {{ playerNames.playerOne }}</span>
+      <span class="text-white mr-4">Player 2: {{ playerNames.playerTwo }}</span>
+    </section>
+  </main>
+</template>
