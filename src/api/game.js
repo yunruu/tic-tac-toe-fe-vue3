d4f3158422211780f29cbd1984bf5a3e7821fecb
yunruu <chin.yunru@flex-solver.com.sg>
2023-09-27 17:27:38 +0800

    feat: add leave game api call

--- src/api/game.js
+++ src/api/game.js
@@ -4,3 +4,5 @@ export const joinGame = async (pid) => http('post', `/game`, { pid })
 
 export const makeMove = async (id, pid, boardIdx, moveValue) =>
   http('patch', `/game/board/${id}/${pid}`, { move: { idx: boardIdx, value: moveValue } })
+
+export const leaveGame = async (id, pid) => http('patch', `/game/${id}/${pid}`)
